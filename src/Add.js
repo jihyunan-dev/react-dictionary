7133a8ff572600626f4f73a89a7e17a30cbacb01
An Jihyun <hwiyu25@gmail.com>
2021-06-28 17:18:56 +0900

    ✨ Add - 단어 추가 기능 생성 및 적용

--- src/Add.js
+++ src/Add.js
@@ -1,7 +1,10 @@
 import React, { useRef } from "react";
+import { useDispatch } from "react-redux";
+import { addWord } from "./redux/module/words";
 import CustomInput from "./CustomInput";
 
-const Add = () => {
+const Add = (props) => {
+  const dispatch = useDispatch();
   const wordRef = useRef(null);
   const pinyinRef = useRef(null);
   const defRef = useRef(null);
@@ -11,6 +14,22 @@ const Add = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     // 이후 dispatch를 사용하여 db 및 store에 저장(add)하는 기능으로 만들기
+    const word = wordRef.current.value.trim();
+    const pinyin = pinyinRef.current.value.trim();
+    const definition = defRef.current.value.trim();
+    const example_cn = exCnRef.current.value.trim();
+    const example_ko = exKoRef.current.value.trim();
+
+    const word_obj = {
+      word,
+      pinyin: pinyin ? pinyin : null,
+      definition,
+      example_cn,
+      example_ko: example_ko ? example_ko : null,
+    };
+
+    dispatch(addWord(word_obj));
+    props.history.push("/");
   };
 
   return (
