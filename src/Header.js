5371b1bb24f415fea23a018226ae0381809cfefa
An Jihyun <hwiyu25@gmail.com>
2021-06-28 23:14:47 +0900

    💄 Header 스타일 적용(반응형 포함)

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,52 @@
 import React from "react";
+import styled, { css } from "styled-components";
+import { Link } from "react-router-dom";
 
-const Header = () => {
+const Header = (props) => {
   return (
-    <div>
-      <h1>중국어 단어장</h1>
-    </div>
+    <HeaderContainer>
+      <Link to="/">
+        <Title>중국어 단어장</Title>
+      </Link>
+    </HeaderContainer>
   );
 };
+
+const HeaderContainer = styled.div`
+  ${({ theme }) => {
+    const { colors, device } = theme;
+    return css`
+      position: fixed;
+      top: 0;
+      left: 0;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 100%;
+      height: 50px;
+      background-color: ${colors.white};
+      border-bottom: 2px solid ${colors.mint};
+
+      ${device.tablet} {
+        height: 60px;
+      }
+    `;
+  }}
+`;
+
+const Title = styled.h1`
+  ${({ theme }) => {
+    const { colors, device, fontSizes } = theme;
+    return css`
+      color: ${colors.mainColor};
+      font-size: ${fontSizes.lg};
+      font-weight: 600;
+
+      ${device.tablet} {
+        font-size: ${fontSizes.xl};
+      }
+    `;
+  }}
+`;
 
 export default Header;
