34829a93c107190e2daf6a28315e31536446512a
An Jihyun <hwiyu25@gmail.com>
2021-06-28 23:15:57 +0900

    💄 WordCard 스타일 적용 (반응형 포함)

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,30 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import WordCard from "./WordCard";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+import WordCard from "./WordCard";
 
 const Home = () => {
   const words = useSelector((state) => state.words.word_list);
   return (
     <div>
-      <div>
+      <Cards>
         {words.map((word) => (
           <WordCard key={word.id} word_obj={word} />
         ))}
-      </div>
+      </Cards>
       <Link to="/word/add">+</Link>
     </div>
   );
 };
+
+const Cards = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  gap: 20px;
+  width: 100%;
+  padding: 50px 0;
+`;
 
 export default Home;
--- src/WordCard.js
+++ src/WordCard.js
@@ -1,17 +1,71 @@
 import React from "react";
+import styled, { css } from "styled-components";
 
 const WordCard = ({ word_obj }) => {
   const { word, pinyin, definition, example_cn, example_ko } = word_obj;
   return (
-    <article>
-      <h4>{word}</h4>
-      <span>[{pinyin}]</span>
-      <p>{definition}</p>
-      <hr />
-      <div>{example_cn}</div>
-      <div>{example_ko}</div>
-    </article>
+    <Card>
+      <WordSet>
+        <Word>{word}</Word>
+        <Pinyin>[{pinyin}]</Pinyin>
+      </WordSet>
+      <Definition>{definition}</Definition>
+      <EXAMPLE>{example_cn}</EXAMPLE>
+      <EXAMPLE>{example_ko}</EXAMPLE>
+    </Card>
   );
 };
+
+const Card = styled.article`
+  ${({ theme }) => {
+    const { colors, device } = theme;
+    return css`
+      width: 100%;
+      padding: 20px;
+      border: 2px solid ${colors.mint};
+      border-radius: 10px;
+      transition: box-shadow 300ms ease-in-out;
+
+      ${device.tablet} {
+        width: calc((100% - 20px) / 2);
+      }
+
+      ${device.desktop} {
+        width: calc((100% - (20px * 2)) / 3);
+      }
+
+      &:hover {
+        box-shadow: rgba(0, 0, 0, 0.15) 0px 5px 15px 0px;
+      }
+    `;
+  }}
+`;
+
+const WordSet = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+`;
+
+const Word = styled.h4`
+  margin-right: 5px;
+  font-size: ${({ theme }) => theme.fontSizes.lg};
+  font-weight: 600;
+`;
+
+const Pinyin = styled.span`
+  font-size: ${({ theme }) => theme.fontSizes.sm};
+`;
+
+const Definition = styled.p`
+  margin-bottom: 10px;
+  font-size: ${({ theme }) => theme.fontSizes.md};
+`;
+
+const EXAMPLE = styled.div`
+  margin-top: 5px;
+  color: ${({ theme }) => theme.colors.blue};
+  font-size: ${({ theme }) => theme.fontSizes.sm};
+`;
 
 export default WordCard;
