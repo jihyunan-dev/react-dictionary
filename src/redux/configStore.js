7e3daa83933b2a943a3c5a4ba10bb64c21bae033
An Jihyun <hwiyu25@gmail.com>
2021-06-28 17:38:01 +0900

    🔧 redux-thunk 미들웨어 스토어에 적용

--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -1,11 +1,15 @@
+import thunk from "redux-thunk";
 import { createBrowserHistory } from "history";
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import words from "./module/words";
 
 export const history = createBrowserHistory();
 
+const middlewares = [thunk];
+
+const enhancer = applyMiddleware(...middlewares);
 const rootReducer = combineReducers({ words });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, enhancer);
 
 export default store;
