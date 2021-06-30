931e716d976a76d37d33e6e576c1e34ed3254e27
An Jihyun <hwiyu25@gmail.com>
2021-06-30 11:41:11 +0900

    💄 Header z-index 적용

--- src/Header.js
+++ src/Header.js
@@ -22,6 +22,7 @@ const HeaderContainer = styled.div`
       position: fixed;
       top: 0;
       left: 0;
+      z-index: 10;
       display: flex;
       justify-content: center;
       align-items: center;
